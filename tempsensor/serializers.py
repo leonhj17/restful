4c1d8876f572a37488c9bf22f38db9ba94f850d9
huangjian <huangjian17@outlook.com>
2018-03-26 19:33:43 +0800

    嵌套型 serializer

--- tempsensor/serializers.py
+++ tempsensor/serializers.py
@@ -1,18 +1,10 @@
 # _*_ encoding:utf-8 _*_
 from rest_framework import serializers
 from .models import TempValue, Sensor
-# class Sensor(models.Model):
-#     sensorKks = models.CharField(max_length=10, default="", null=False)
-#     x = models.FloatField(max_length=6, default=-1.0)
-#     y = models.FloatField(max_length=6, default=-1.0)
-#     row = models.IntegerField(default=-1)
-#     column = models.IntegerField(default=-1)
-#
-#     class Meta:
-#         ordering = ("sensorKks",)
 
 
 class SensorSerializer(serializers.Serializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)
     sensorKks = serializers.CharField(max_length=10)
     x = serializers.FloatField()
     y = serializers.FloatField()
@@ -21,7 +13,8 @@ class SensorSerializer(serializers.Serializer):
 
 
 class TempValueSerializer(serializers.Serializer):
-    sensorKks = serializers.StringRelatedField(read_only=True)
+    # id = serializers.PrimaryKeyRelatedField(read_only=True)
+    sensorKks = SensorSerializer()
     value = serializers.FloatField()
     time = serializers.DateTimeField()
 
