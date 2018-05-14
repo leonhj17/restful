7219e8bc03d5d9c498a9725f6b3dd6f043bfc5fc
leon17hj <huangjian17@outlook.com>
2018-05-14 21:21:37 +0800

    celery 添加 定时任务，用以生成截面模拟温度

--- tempsensor/tasks.py
+++ tempsensor/tasks.py
@@ -1,5 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from .models import Sensor, TempValue, TempCenter
+from .serializers import SensorSerializer
+import numpy as np
+from random import random, seed
+from pandas import DataFrame as df
+from datetime import datetime
+from csv_to_db import add_db, read_value_csv
 
 
 @shared_task
@@ -20,3 +27,23 @@ def interval_task():
 @shared_task
 def crontab_task():
     print 'crontab task execute'
+
+
+@shared_task
+def simulate_gastemp():
+    obj = Sensor.objects.all()
+    serializer = SensorSerializer(obj, many=True)
+    data = serializer.data
+
+    def type_sin(param):
+        seed()
+        # 21480: boiler width
+        return 1 + np.sin(param / 21480 * np.pi) * 0.3 * ((1 - 2 * random()) / 5 + 1)
+
+    data_df = df(data, columns=['sensorKks', 'x', 'y', 'value', 'time'])
+    data_df['value'] = type_sin(data_df['x'])*type_sin(data_df['y'])*700
+    data_df['time'] = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+    data_df.to_csv('gastemp.csv', index=False, header=False, columns=['sensorKks', 'value', 'time'])
+
+    add_db(read_value_csv, TempValue, 'gastemp.csv')
