22b1c2278190e8a80a5677aa84ea14308d2f712f
huangjian <huangjian17@outlook.com>
2018-07-05 16:44:49 +0800

    烟温基础概况 （增加扇形图）

--- tempsensor/urls.py
+++ tempsensor/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     url(r'home/$', TemplateView.as_view(template_name='page_home.html'), name='home'),
     url(r'get_json_tempvalue/$', views.get_json_tempvalue, name='get_json_tempvalue'),
     url(r'^tempcenter/(?P<duration>[0-9]+)/$', views.TempCenterList.as_view(), name='tempcenterlist'),
-    url(r'^highcharts/(?P<id>[0-9]+)/$', views.highcharats_get_kksid)
+    url(r'^highcharts/(?P<id>[0-9]+)/$', views.highcharats_get_kksid),
+    url(r'basicoverview/$', TemplateView.as_view(template_name='basicoverview.html'), name='basicoverview')
 ]
